473e30897ef4ecab6335b0cc87d309709910aa8a
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-07-03 17:11:37 +0200

    Use a different database in regular dev.

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ use Mix.Config
 
 # Using the second database /1 for tests, but pubsub still uses the regular database
 # due to limitations in :eredis.
-config :exqueue, redis_url: "redis://localhost:6379/1"
+if Mix.env == :test do
+  config :exqueue, redis_url: "redis://localhost:6379/1"
+else
+  config :exqueue, redis_url: "redis://localhost:6379/0"
+end
