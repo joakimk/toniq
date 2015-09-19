9ec3090ab8d051c711860fd092f4bf8bf5df7232
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-09-19 14:55:39 +0200

    Remove full stack failover test and replace with simpler test

--- test/toniq/failover_test.exs
+++ test/toniq/failover_test.exs
@@ -1,44 +1,60 @@
+# Ensure we can failover jobs from one VM to another when it exits or crashes
 defmodule Toniq.FailoverTest do
   use ExUnit.Case
 
   setup do
-    Process.whereis(:toniq_redis) |> Exredis.query([ "FLUSHDB" ])
+    Process.whereis(:toniq_redis) |> Exredis.query(["FLUSHDB"])
+    :ok
+  end
 
-    Toniq.FailoverTestHelper.spawn_and_connect("one")
-    Toniq.FailoverTestHelper.spawn_and_connect("two")
+  defmodule FakeWorker do
+    use Toniq.Worker
 
-    Toniq.FailoverTestHelper.add_job("one")
-    Toniq.FailoverTestHelper.add_job("two")
-
-    on_exit fn ->
-      Toniq.FailoverTestHelper.halt("one")
-      Toniq.FailoverTestHelper.halt("two")
+    def perform do
+      send :failover_test, :inherited_job_was_run
     end
   end
 
-  test "scopes jobs to each erlang vm and inherits jobs when one vm stops" do
-    one = Toniq.FailoverTestHelper.get_state("one")
-    two = Toniq.FailoverTestHelper.get_state("two")
+  # This test is simplified so we don't need to run multiple erlang vms (it's tricky and
+  # leads to unreliable tests). Instead of multiple VMs we simply persist a job as-if
+  # there was another VM (different key names in redis) and then stop the keepalive for
+  # that fake VM and ensure the job is taken over by the test VM.
+  test "orphaned jobs are taken over and run" do
+    Process.register self, :failover_test
 
-    # Ensure we have two new system processes running
-    assert one.system_pid != nil
-    assert two.system_pid != nil
-    assert one.system_pid != two.system_pid
+    current_vm = Toniq.Keepalive.identifier
+    other_vm = start_keepalive(:other_vm)
 
-    # WIP:
+    # Add job to other_vm and check that it only exists there
+    add_job(other_vm)
+    assert Enum.count(Toniq.JobPersistence.jobs(current_vm)) == 0
+    assert Enum.count(Toniq.JobPersistence.jobs(other_vm)) == 1
+
+    # Stop keepalive for other_vm and sure the job is picked up and run.
+    # We assume it's run in current_vm here since there is no easy way to check.
+    stop_keepalive(:other_vm)
+    timeout = 500 # ms
+    assert_receive :inherited_job_was_run, timeout
   end
-  #  assert Enum.count(one.jobs) == 1
-  #  assert Enum.count(two.jobs) == 1
-  #
-  #  Toniq.FailoverTestHelper.halt("one")
-  #
-  #  :timer.sleep 1000
-  #  assert Enum.count(two.jobs) == 2
-  #
-  #  Toniq.FailoverTestHelper.halt("two")
-  #  Toniq.FailoverTestHelper.spawn_and_connect("one")
-  #
-  #  :timer.sleep 1000
-  #  assert Enum.count(one.jobs) == 2
-  #end
+
+  defp add_job(identifier) do
+    Toniq.JobPersistence.store_job(FakeWorker, [], identifier)
+  end
+
+  defp start_keepalive(name) do
+    {:ok, _pid} = Toniq.Keepalive.start_link(name)
+    Toniq.Keepalive.identifier(name)
+  end
+
+  defp stop_keepalive(name) do
+    name
+    |> Process.whereis
+    |> unlink_process
+    |> Process.exit(:kill)
+  end
+
+  defp unlink_process(pid) do
+    true = Process.unlink(pid)
+    pid
+  end
 end
