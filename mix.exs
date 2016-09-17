5c6ad161b3b78ca20f1232e9f53a788979479df8
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-09-17 16:21:08 +0200

    Up version to 1.1.0
    Since 1.0.7 stack traces have been added to error reporting. It's not a
    bug fix or a breaking change so upping the minor version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Toniq.Mixfile do
 
   def project do
     [app: :toniq,
-     version: "1.0.7",
+     version: "1.0.8",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
